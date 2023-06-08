cc7fc14aedd80bea69280e345b39ee226cb4314d
helenhe40 <yhe@lbl.gov>
2023-06-08 00:30:32 -0700

    update 01-hello

--- 01-hello/run-hello-cpu-2.sh
+++ 01-hello/run-hello-cpu-2.sh
@@ -5,4 +5,12 @@
 #SBATCH -t 00:10:00
 #SBATCH -J my_job
 
+echo "use the default gnu compiler"
+module load cpu
+cc -o mpi-hello mpi-hello.c
+srun -n 8 -c 32 --cpu-bind=cores ./mpi-hello |sort -k4
+
+echo "use the cce compiler"
+module load PrgEnv-cray
+ftn -o mpi-hello mpi-hello.f90
 srun -n 8 -c 32 --cpu-bind=cores ./mpi-hello |sort -k4
